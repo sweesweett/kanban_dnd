509477a19eafd0a1bbe767f81d860491d62c3bc0
sweesweett <sweesweet@naver.com>
2023-01-21 23:20:14 +0900

    feat:#02 컴포넌트 스타일링

--- src/components/taskList/TaskBoard.tsx
+++ src/components/taskList/TaskBoard.tsx
@@ -1,11 +1,26 @@
+import styled from 'styled-components';
 import TaskList from './TaskList';
+import TaskTitleForm from './TaskTitleForm';
 const TaskBoard = () => {
   return (
-    <>
-      <TaskList />
-      <TaskList />
-      <TaskList />
-    </>
+    <TaskBoardWrapper>
+      <TaskTitleForm />
+      <TaskListWrapper>
+        <TaskList />
+        <TaskList />
+        <TaskList />
+      </TaskListWrapper>
+    </TaskBoardWrapper>
   );
 };
+const TaskBoardWrapper = styled.section`
+  margin: 50px 30px;
+  min-height: 80vh;
+  background-color: rgba(255, 255, 255, 0.4);
+`;
+const TaskListWrapper = styled.div`
+  margin: 20px;
+  display: flex;
+  gap: 30px;
+`;
 export default TaskBoard;
--- src/components/taskList/TaskList.tsx
+++ src/components/taskList/TaskList.tsx
@@ -1,8 +1,18 @@
+import styled from 'styled-components';
 const TaskList = () => {
   return (
-    <ul>
-      <li>테스트중~</li>
-    </ul>
+    <TaskListContainer>
+      <h2>타이틀</h2>
+      <TaskListUl>
+        <li>테스트중~</li>
+      </TaskListUl>
+    </TaskListContainer>
   );
 };
+const TaskListContainer = styled.div`
+  min-width: 200px;
+  height: 500px;
+  background-color: rgba(255, 255, 255, 0.7);
+`;
+const TaskListUl = styled.ul``;
 export default TaskList;
