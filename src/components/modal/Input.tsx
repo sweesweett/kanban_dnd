a09d6c9bbfe9263c469748090a86f56aa566680f
sweesweett <sweesweet@naver.com>
2023-01-29 15:59:46 +0900

    design:#11 Input 스타일링 추가

--- src/components/modal/Input.tsx
+++ src/components/modal/Input.tsx
@@ -1,9 +1,29 @@
+import styled from 'styled-components';
+
 const Input = ({ type, name, label, options }: { type: string; name: string; label: string; options: {} }) => {
   return (
-    <div>
-      <label htmlFor={name}>{label}</label>
-      <input type={type} name={name} {...options} />
-    </div>
+    <InputWrapper>
+      <Label htmlFor={name}>{label}</Label>
+      <InputEl type={type} name={name} {...options} />
+    </InputWrapper>
   );
 };
+
+const InputWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 12px;
+  padding: 8px 0;
+`;
+
+const Label = styled.label`
+  font-weight: 700;
+`;
+const InputEl = styled.input`
+  :focus {
+    background-color: rgba(255, 255, 255, 0.8);
+    outline: 2px solid rgba(0, 0, 0, 0.3);
+  }
+`;
+
 export default Input;
