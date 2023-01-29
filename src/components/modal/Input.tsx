b51a9a5ab1bf1917c733ada47eb9ab5a3b0448c2
sweesweett <sweesweet@naver.com>
2023-01-29 23:12:03 +0900

    design:#11 Modal 컴포넌트 1차 스타일링

--- src/components/modal/Input.tsx
+++ src/components/modal/Input.tsx
@@ -12,18 +12,22 @@ const Input = ({ type, name, label, options }: { type: string; name: string; lab
 const InputWrapper = styled.div`
   display: flex;
   align-items: center;
-  gap: 12px;
   padding: 8px 0;
+  width: 100%;
 `;
 
 const Label = styled.label`
   font-weight: 700;
+  margin-right: 12px;
+  width: 80px;
 `;
 const InputEl = styled.input`
+  font-size: ${(props) => (props.size ? props.size : 16)}px;
   :focus {
     background-color: rgba(255, 255, 255, 0.8);
     outline: 2px solid rgba(0, 0, 0, 0.3);
   }
+  width: 100%;
 `;
 
 export default Input;
