d07f85ff651fe4b34afbd8db925532fe2153c396
Yunjin Lee <sweejin@naver.com>
2023-02-13 16:24:09 +0900

    refactor:#15 TaskTitleForm network err 발생시 이전값으로 롤백되도록 구현, 칸반보드 제목 input => h2로 변경

--- src/components/taskList/TaskTitleForm.tsx
+++ src/components/taskList/TaskTitleForm.tsx
@@ -1,14 +1,14 @@
-import React, { FocusEvent, FormEvent } from 'react';
+import { FocusEvent, FormEvent, useRef } from 'react';
 import styled from 'styled-components';
 import { graphqlFetcher } from '../../queryClient';
 import { useMutation } from 'react-query';
 import { RequestDocument } from 'graphql-request';
-import { useRecoilState } from 'recoil';
+import { useSetRecoilState } from 'recoil';
 import { listNameSelector } from '../../store';
-import { StateChange } from '../../types/lists';
 
 const TaskTitleForm = ({ size, title, eventName }: { size: number; title: string; eventName: RequestDocument }) => {
-  const [titles, setTitles] = useRecoilState(listNameSelector);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const setTitles = useSetRecoilState(listNameSelector);
   const fetcher = useMutation((newState: string) => graphqlFetcher(eventName, { state: title, newState }), {
     onSuccess: (data) => {
       const { state } = data as { state: string };
@@ -16,26 +16,27 @@ const TaskTitleForm = ({ size, title, eventName }: { size: number; title: string
     },
     onError: (err: string) => {
       console.log(`Error:${err}`);
+      if (inputRef.current) {
+        inputRef.current.value = title;
+      }
     },
   });
+
   const submitHandler = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const title = e.currentTarget.titleVal as HTMLFormElement;
     const titleValue = title.value as string;
     fetcher.mutate(titleValue);
-
-    // TODO:리액트 쿼리 useMutation 사용방법 더 찾아보고 리팩토링 하기
   };
   const blurHandler = (e: FocusEvent<HTMLInputElement, Element>) => {
     if (e.target.value === title) {
       return;
     }
     fetcher.mutate(e.target.value);
-    // TODO:리액트 쿼리 useMutation 사용방법 더 찾아보고 리팩토링 하기
   };
   return (
     <TitleForm onSubmit={submitHandler}>
-      <TitleInput name="titleVal" onBlur={blurHandler} type="text" size={size} defaultValue={title} />
+      <TitleInput name="titleVal" onBlur={blurHandler} type="text" size={size} defaultValue={title} ref={inputRef} />
     </TitleForm>
   );
 };
