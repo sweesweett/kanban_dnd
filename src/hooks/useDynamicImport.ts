4a5ac373d2046debab3d8a2dfa1e69fdabc6870b
Yunjin Lee <sweejin@naver.com>
2023-02-14 13:15:14 +0900

    style:#15 useDynamicImport 코드 변경(기능엔 차이 없음)

--- src/hooks/useDynamicImport.ts
+++ src/hooks/useDynamicImport.ts
@@ -2,13 +2,15 @@ import { RequestDocument } from 'graphql-request';
 import { useState, useEffect } from 'react';
 
 const useDynamicImport = (mode: string) => {
-  const [query, setquery] = useState<RequestDocument>('');
+  const [query, setQuery] = useState<RequestDocument>('');
   const dynamicImport = async (mode: string) => {
-    if (mode === 'edit') {
-      await import('../graphql/lists').then((q) => setquery(q.PUT_ITEM));
-    } else {
-      await import('../graphql/lists').then((q) => setquery(q.POST_ITEM));
-    }
+    await import('../graphql/lists').then((q) => {
+      if (mode === 'edit') {
+        return setQuery(q.PUT_ITEM);
+      }
+
+      return setQuery(q.POST_ITEM);
+    });
   };
 
   useEffect(() => {
